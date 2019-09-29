78aec7f77ed1f3e3ce31666a3fa64f02c94ee0b2
Javier Fonseca <fonsecajavier1989@gmail.com>
2019-09-29 18:22:14 -0500

    Configuración inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-less/semantic.less';
+
+import Login from './Pages/Login';
+import Divisas from './Pages/Divisas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/divisas" component={Divisas} />
+      </Container>
+    </Router>
   );
 }
 
